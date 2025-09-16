8911b056a4736bf168bd9dd65f2de4d9ae10f1b0
Ford Henley <info@fordswebhelp.com>
2025-09-16 15:53:32 -0400

    completed method overloading challenge

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,43 @@
 {
     public class Program
     {
+        //define methods to overload
+        public static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static decimal Add(decimal numOne, decimal numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static string Add(int numOne, int numTwo, bool checkIsTrue)
+        {
+            var sum = numOne + numTwo;
+            
+            if (checkIsTrue == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (checkIsTrue == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else if (checkIsTrue == true && sum < 1)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+            
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(2.0m,2.0m));
         }
     }
 }
